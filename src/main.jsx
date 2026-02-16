d19e3ca861ae7c8524c65a22ab79caf4e66a8583
Radu Bobirnac <radubobirnac@github.com>
2026-02-16 09:01:04 -0500

    fix: update .gitignore to include .idea and .log directories; add test comment in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
-
+//TEST TO SEE IF I CAN COMMIT CHANGES TO GITHUB
 const root = createRoot(document.getElementById("root"));
 
 root.render(
